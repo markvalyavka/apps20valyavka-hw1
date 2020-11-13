bfd927068b129216dfe825675a5245ccca07f94c
Mark Valyavka <v221101mark@gmail.com>
2020-11-13 05:50:52 +0200

    implemented first 5 functions

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,37 +1,92 @@
 package ua.edu.ucu.tempseries;
+import java.lang.Math;
 
 public class TemperatureSeriesAnalysis {
+    double [] temperatureSeries;
 
     public TemperatureSeriesAnalysis() {
 
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-
+        this.temperatureSeries = temperatureSeries;
     }
 
     public double average() {
-        return -1;
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        double tempSum = 0;
+        for (double temperature : temperatureSeries) {
+            tempSum += temperature;
+        }
+        return tempSum / temperatureSeries.length;
     }
 
     public double deviation() {
-        return 0;
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        double mean = average();
+        double deavSum = 0;
+        for (double temperature : temperatureSeries) {
+            deavSum += Math.pow(temperature - mean, 2) ;
+        }
+        return Math.pow(deavSum / temperatureSeries.length, 0.5);
     }
 
     public double min() {
-        return 0;
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        double minTemp = temperatureSeries[0];
+        for (double temperature : temperatureSeries) {
+            minTemp = Math.min(minTemp, temperature);
+        }
+        return minTemp;
     }
 
     public double max() {
-        return 0;
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        double maxTemp = temperatureSeries[0];
+        for (double temperature : temperatureSeries) {
+            maxTemp = Math.min(maxTemp, temperature);
+        }
+        return maxTemp;
     }
 
     public double findTempClosestToZero() {
-        return 0;
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        double closestToZeroTemp = temperatureSeries[0];
+        for (double temperature : temperatureSeries) {
+            double toZeroValue = Math.abs(temperature);
+            if (toZeroValue < Math.abs(closestToZeroTemp)) {
+                closestToZeroTemp = temperature;
+            } else if (toZeroValue == Math.abs(closestToZeroTemp)) {
+                closestToZeroTemp = Math.max(closestToZeroTemp, temperature);
+            }
+        }
+        return closestToZeroTemp;
     }
 
     public double findTempClosestToValue(double tempValue) {
-        return 0;
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        double closestToValueTemp = temperatureSeries[0];
+        for (double temperature : temperatureSeries) {
+            double toValue = Math.abs(temperature - tempValue);
+            if (toValue < Math.abs(closestToValueTemp - tempValue)) {
+                closestToValueTemp = temperature;
+            } else if (toValue == Math.abs(closestToValueTemp - tempValue)) {
+                closestToValueTemp = Math.max(closestToValueTemp, temperature);
+            }
+        }
+        return closestToValueTemp;
     }
 
     public double[] findTempsLessThen(double tempValue) {
